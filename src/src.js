a73a34dd24c0fcfc273fc5e77972778ad645d0e9
Philip Young <philip.young.gunawan@gmail.com>
2016-12-28 03:23:00 +0700

    UI updates

--- src/src.js
+++ src/src.js
@@ -2,14 +2,14 @@
   const sortAllBy = R.path(['color']);
 
   const app = {
-    text: ['Guns, Germs, and Steel.'],
     fontFamily: ['Lato', 'Avenir', 'Georgia', 'Playfair Display', 'Times New Roman'],
     fontSize: ['3rem'],
-    color: ['black', 'mediumblue'],
+    color: ['#333', 'mediumblue'],
     lineHeight: ['1.2'],
     marginTop: ['0'],
-    marginBottom: ['0.5rem'],
+    marginBottom: ['8px'],
     letterSpacing: ['-0.025em'],
+    text: ['Guns, Germs, and Steel'],
     description: {
       text: ["So the first number of the result is easy, a, b, and c, are all the first elements of each array. The second one isn't as easy for me to understand. Are the arguments the second value of each array (2, 2, undefined) or is it the second value of the first array and the first values of the second and third array?"],
       fontFamily: ['sans-serif', 'Georgia'],
@@ -17,7 +17,7 @@
       color: ['#333'],
       lineHeight: ['1.5'],
       marginTop: ['0'],
-      marginBottom: ['6rem'],
+      marginBottom: ['0'],
       letterSpacing: ['0'],
     },
     subTitle: {
@@ -51,28 +51,35 @@
     insideCombination,
   ]);
 
-  R.forEach(key => {
-    $('#inputs')
-      .append($('<div>')
-        .css({
-          marginBottom: '1rem',
-        })
-        .append($('<label>', {
-            text: key,
-          })
+  // ///////////////////////////////////////////////////////////////////////////
+
+  const renderInputs = () => {
+    R.forEach(key => {
+      $('#inputs')
+        .append($('<div>')
           .css({
-            display: 'block',
-            fontSize: '0.75rem',
+            marginBottom: '1rem',
           })
-        )
-        .append($('<input>', {
-          value: String(app[key]).replace(/,(?=[^\s])/g, ', '),
-        }))
-      );
-  })(R.take(propLength)(R.keys(app)));
+          .append($('<label>', {
+              text: key,
+            })
+            .css({
+              display: 'block',
+              fontSize: '0.75rem',
+            })
+          )
+          .append($('<input>', {
+            value: String(app[key]).replace(/,(?=[^\s])/g, ', '),
+            name: key,
+          }))
+        );
+    })(R.take(propLength)(R.keys(app)));
+  }
 
   const renderPermutation = () => {
     const data = allCombination(app);
+    $('#total-combinations')
+      .text(`${data.length} total combinations`)
     R.sortBy(sortAllBy)(data).forEach((item, index) => {
       const {
         text,
@@ -85,56 +92,70 @@
         subTitle,
         letterSpacing,
       } = item;
+
       $('#app')
-        .append($('<p>')
+        .append($('<p>', {
+            class: 'typography-item-identifier',
+          })
           .text(`${index + 1}. ${item.fontFamily} - ${item.color} - ${item.fontSize}`)
-          .css({
-            borderBottom: '1px solid silver',
-            marginBottom: '1rem',
-          })
         )
-        .append($('<h1>')
-          .text(`${text}`)
-          .css({
-            fontFamily,
-            color,
-            fontSize,
-            lineHeight,
-            marginBottom,
-            letterSpacing,
+        .append($('<div>', {
+            class: 'typography-item',
           })
-        )
-        .append($('<p>')
-          .text(`${subTitle.text}`)
-          .css({
-            fontFamily: subTitle.fontFamily,
-            color: subTitle.color,
-            fontSize: subTitle.fontSize,
-            lineHeight: subTitle.lineHeight,
-            marginBottom: subTitle.marginBottom,
-          })
-        )
-        .append($('<p>')
-          .text(`${description.text}`)
-          .css({
-            fontFamily: description.fontFamily,
-            color: description.color,
-            fontSize: description.fontSize,
-            lineHeight: description.lineHeight,
-            marginBottom: description.marginBottom,
-          })
-        );
+          .append($('<h1>')
+            .text(`${text}`)
+            .css({
+              fontFamily,
+              color,
+              fontSize,
+              lineHeight,
+              marginBottom,
+              letterSpacing,
+            })
+          )
+          .append($('<p>')
+            .text(`${subTitle.text}`)
+            .css({
+              fontFamily: subTitle.fontFamily,
+              color: subTitle.color,
+              fontSize: subTitle.fontSize,
+              lineHeight: subTitle.lineHeight,
+              marginBottom: subTitle.marginBottom,
+            })
+          )
+          .append($('<p>')
+            .text(`${description.text}`)
+            .css({
+              fontFamily: description.fontFamily,
+              color: description.color,
+              fontSize: description.fontSize,
+              lineHeight: description.lineHeight,
+              marginBottom: description.marginBottom,
+            })
+          ));
     });
   };
 
   // ///////////////////////////////////////////////////////////////////////////
 
+  renderInputs();
   renderPermutation();
 
-  $('input').on('keyup', e => {
-    const prop = $(e.target).prev().text();
+  $('input').each((index, inp) => {
+    $(inp).selectize({
+      delimiter: $('input').attr('name') === 'text' ? '>' : ',',
+      create: input => ({
+        value: input,
+        text: input,
+      }),
+    });
+  });
+
+  $('input').on('change', e => {
+    const prop = $(e.target).prop('name');
     const value = R.split(',', R.trim($(e.target).val()));
-    app[prop] = value;
+    app[prop] = prop === 'text' ? [$(e.target).val()] : value;
+
     $('#app').empty();
     renderPermutation();
   });
